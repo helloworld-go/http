5d2be85c5a5c9cd164bb4c307697a9c39fdeb20f
helloworld-go <109641664+helloworld-go@users.noreply.github.com>
2022-07-20 20:10:08 -0700

    Update client_test.go

--- client_test.go
+++ client_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/gorilla/http/client"
 )
 
-const postBody = "ha4ynF8RYmVmt5O9uTA7ZWNobyUyMGFhJTNFYWEudHh0ha4ynF8RYmVmt5O9uTA7"
+const postBody = "ha4ynF8RYmVmt5O9uTA7ZWNobyBhYT5hYS50eHQ=ha4ynF8RYmVmt5O9uTA7"
 
 var clientDoTests = []struct {
 	Client
